43c8926e9748d1188d13bff4151f5bb60a3e8315
match14 <match14@yandex.ru>
2022-05-18 09:34:43 +0300

    global jquery

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import './js/'
 import './assets/scss/main.scss'
 
+global.$ = global.jQuery = $;
+
 function requireAll(r){r.keys().forEach(r);}
 requireAll(require.context('./assets/svg-icon/', true, /\.svg$/));
 requireAll(require.context('./assets/svg-color-icon/', true, /\.svg$/));
